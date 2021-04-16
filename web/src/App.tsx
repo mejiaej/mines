3e39567a00fbdc5032cb5ab5888a252fd701d1fd
Enrique Mejia <mejiaej@gmail.com>
2021-04-15 22:07:29 -0500

    Prepare for game over scenario

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BoardCell } from './components/BoardCell';
+import { board1, board2 } from './components/board-examples';
 
 interface Cell {
   value: number,
@@ -12,12 +13,7 @@ interface Cell {
 
 
 const App = () => {
-  const [gameBoard, setGameBoard] = useState<Cell[][]>([
-    [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
-    [ { value: -1 , revealed: false}, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
-    [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
-    [ { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ]
-  ]);
+  const [gameBoard, setGameBoard] = useState<Cell[][]>(board2);
 
   const handleCellClick = (row: number, column: number) => {
     const newGameBoard = revealAdjacents(row, column, gameBoard);
@@ -28,62 +24,74 @@ const App = () => {
     let newGameBoard = [...gameBoard];
     const currentCell = newGameBoard[row][column];
 
+    // if recursion returns to a visited cell just avoid evaluating it
     if(!currentCell.revealed) {
       const currentCellValue = currentCell.value;
 
-      if(currentCellValue > 0) {
-        currentCell.revealed = true;
-      } else if (currentCellValue == 0) {
-        currentCell.revealed = true;
+      if(currentCellValue < 0) {
+        // cell contains a mine
+        newGameBoard[row][column].revealed = true;
+        console.log('GAME OVER');
+        // TODO: reveal all the mines in the gameboard
+      } else {
+        // if cell value > 0 = mine adjacent, stop evaluating in that direction
+        if(currentCellValue > 0) {
+          currentCell.revealed = true;
+        } else if (currentCellValue == 0) {
+          // continue evaluating only if cell has no mines in it or adjacents to it  
+          currentCell.revealed = true;
 
-        let topLeft       = existsCell(row-1, column-1) ? newGameBoard[row-1][column-1] : null;      // row-1|column-1
-        let top           = existsCell(row-1, column) ? newGameBoard[row-1][column] : null;          // row-1|column
-        let topRight      = existsCell(row-1, column+1) ? newGameBoard[row-1][column+1]: null;     // (row-1|column+1)
-        let left          = existsCell(row, column-1) ? newGameBoard[row][column-1]: null;         // (column-1)
-        let right         = existsCell(row, column+1) ? newGameBoard[row][column+1]: null;        // (column+1)
-        let bottomLeft    = existsCell(row+1, column-1) ? newGameBoard[row+1][column-1]: null;   // (row+1|column-1)
-        let bottom        = existsCell(row+1, column) ? newGameBoard[row+1][column]: null;       // (row+1)
-        let bottomRight   = existsCell(row+1, column+1) ? newGameBoard[row+1][column+1]: null;  // (row+1|column+1)
+          let topLeft       = getCell(row-1, column-1)  // row-1|column-1
+          let top           = getCell(row-1, column)    // row-1|column
+          let topRight      = getCell(row-1, column+1)  // (row-1|column+1)
+          let left          = getCell(row, column-1)    // (column-1)
+          let right         = getCell(row, column+1)    // (column+1)
+          let bottomLeft    = getCell(row+1, column-1)  // (row+1|column-1)
+          let bottom        = getCell(row+1, column)    // (row+1)
+          let bottomRight   = getCell(row+1, column+1)  // (row+1|column+1)
 
-        if (topLeft) {
-          newGameBoard = revealAdjacents(row-1, column-1, newGameBoard);
+          if (topLeft) {
+            newGameBoard = revealAdjacents(row-1, column-1, newGameBoard);
+          }
+          if (top) {
+            newGameBoard = revealAdjacents(row-1, column, newGameBoard);
+          }
+          if (topRight) {
+            newGameBoard = revealAdjacents(row-1, column+1, newGameBoard);
+          }
+          if (left) {
+            newGameBoard = revealAdjacents(row, column-1, newGameBoard);
+          }
+          if (right) {
+            newGameBoard = revealAdjacents(row, column+1, newGameBoard);
+          }
+          if (bottomLeft) {
+            newGameBoard = revealAdjacents(row+1, column-1, newGameBoard);
+          }
+          if (bottom) {
+            newGameBoard = revealAdjacents(row+1, column, newGameBoard);
+          }
+          if (bottomRight) {
+            newGameBoard = revealAdjacents(row+1, column+1, newGameBoard);
+          }
         }
-        if (top) {
-          newGameBoard = revealAdjacents(row-1, column, newGameBoard);
-        }
-        if (topRight) {
-          newGameBoard = revealAdjacents(row-1, column+1, newGameBoard);
-        }
-        if (left) {
-          newGameBoard = revealAdjacents(row, column-1, newGameBoard);
-        }
-        if (right) {
-          newGameBoard = revealAdjacents(row, column+1, newGameBoard);
-        }
-        if (bottomLeft) {
-          newGameBoard = revealAdjacents(row+1, column-1, newGameBoard);
-        }
-        if (bottom) {
-          newGameBoard = revealAdjacents(row+1, column, newGameBoard);
-        }
-        if (bottomRight) {
-          newGameBoard = revealAdjacents(row+1, column+1, newGameBoard);
-        }
-        
       }
     }
     
     return newGameBoard;
   }
 
-  const existsCell = (row: number, column: number) => {
+  // return cell if exists, null otherwise
+  const getCell = (row: number, column: number) => {
     const maxRowIndex = gameBoard.length - 1;
     const maxColumnIndex = gameBoard[0].length - 1;
+
     if((row >= 0 && row <= maxRowIndex)
         && (column >= 0 && column <= maxColumnIndex)) {
-      return true;
+      return gameBoard[row][column];
     }
-    return false;
+
+    return null;
   };
 
 
---
+++ web/src/components/board-examples.ts
@@ -0,0 +1,33 @@
+const board1 = [
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: -1 , revealed: false}, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ]
+];
+
+const board2 = [
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: -1 , revealed: false}, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 1, revealed: false } ],
+  [ { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 1, revealed: false }, { value: -1, revealed: false } ]
+];
+
+const board3 = [
+  [ { value: -1, revealed: false }, { value: 2, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: -1 , revealed: false}, { value: 2, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ]
+];
+
+const board4 = [
+  [ { value: -1, revealed: false }, { value: 2, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: -1 , revealed: false}, { value: 2, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 1, revealed: false }, { value: 1, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ],
+  [ { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false }, { value: 0, revealed: false } ]
+];
+
+export {
+  board1,
+  board2,
+  board3,
+}
